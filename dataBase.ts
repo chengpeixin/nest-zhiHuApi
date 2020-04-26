ca3098256dc452a5d9b5629481d1c9009df028cc
辛培铖 <xinpeicheng@yeah.net>
2020-04-26 14:46:01 +0800

    修改数据库面膜

--- dataBase.ts
+++ dataBase.ts
@@ -1,7 +1,8 @@
-export const mongodbUrl = 'mongodb://39.106.209.230:27017/zhihu'
+export const mongodbUrl = '数据库地址'
+// export const mongodbUrl = 'mongodb://39.106.209.230:27017/zhihu'
 
-export const pwd = 'xpc@123'
+export const pwd = '数据库密码'
 
-export const account = 'root'
+export const account = '数据库账号'
 
-export const authSource = 'admin'
+export const authSource = '数据库权限'
