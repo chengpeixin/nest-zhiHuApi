ce1b57dd17d35a54d2736626e08c56180c02c657
辛培铖 <xinpeicheng@yeah.net>
2020-05-08 16:37:33 +0800

    增加日志系统Winston

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,28 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
 import {PaginateParseIntPipe} from './pipes/paginate.pipe'
+import { WinstonModule } from 'nest-winston'
+import * as Winston from 'winston'
+/**
+ * {
+    logger:WinstonModule.createLogger({
+      level: 'info',
+      format: Winston.format.json(),
+      defaultMeta: { service: 'user-service' },
+      transports: [
+        //
+        // - Write all logs with level `error` and below to `error.log`
+        // - Write all logs with level `info` and below to `combined.log`
+        //
+        new Winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new Winston.transports.File({ filename: 'combined.log' })
+      ]
+    })
+  }
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,);
+  // app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER))
   app.useGlobalPipes(new PaginateParseIntPipe())
   app.useGlobalPipes(new ValidationPipe(
     {
