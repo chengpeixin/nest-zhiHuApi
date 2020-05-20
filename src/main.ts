633969a647930ed1dd75f8fe6517f60df840c95e
辛培铖 <xinpeicheng@yeah.net>
2020-05-21 02:43:20 +0800

    限制获取接口速率，防止爬虫

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { TransformInterceptor } from './interceptor/transform.interceptor';
-import {PaginateParseIntPipe} from './pipes/paginate.pipe'
-import {getLogger} from './lib/Logger'
-
+import { PaginateParseIntPipe } from './pipes/paginate.pipe'
+import { getLogger } from './lib/Logger'
+import * as rateLimit from 'express-rate-limit'
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule,{
     logger:getLogger()
   });
+  app['set']('trust proxy', 1)
+  app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }))
   app.useGlobalPipes(new PaginateParseIntPipe())
   app.useGlobalPipes(new ValidationPipe(
     {
