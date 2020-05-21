28ba00d778813398402d479a43d8946cabc8c444
辛培铖 <xinpeicheng@yeah.net>
2020-05-21 20:19:45 +0800

    1. 修改模板文件

--- config.example.ts
+++ config.example.ts
@@ -8,3 +8,16 @@ export const authSource = '数据库权限'
 
 
 export const secret = 'pwd加密key'
+
+export const redisOpts = {
+    port:'redis 端口号',
+    host:'redis IP地址',
+    password:'redis 密码',
+    db:'redis db'
+}
+
+// 默认的redis存储的token有效期
+export const tokenEx = 60*60*24*7
+
+// jwt有效期
+export const jwtEx = '7d'
